3bf2ce91ec3ce0d47c5c247c12f2148843541c20
Luiz Felipe UESC <luizphelip1998@gmail.com>
2026-03-28 05:31:21 -0300

    Commit inicial

---
+++ pl.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include "tokens.hpp"
+
+/*  TODO: fazer a leitura de um arquivo .txt, e chamar o lexer yyFlexLexer para identificar os tokens (OLHAR PÁGINA 35)*/
---
+++ teste.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "tokens.hpp"
+
+int main(){
+    std::cout << ATT << std::endl;
+    
+    return 0;
+}
