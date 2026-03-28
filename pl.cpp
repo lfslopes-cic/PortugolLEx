278292421e7e33dd93bf8a6fda7e0182ddab872e
Luiz Felipe UESC <lfslopes.cic@uesc.br>
2026-03-28 05:33:30 -0300

    Adicionando TODO

--- pl.cpp
+++ pl.cpp
@@ -1,4 +1,4 @@
 #include <iostream>
 #include "tokens.hpp"
 
-/*  TODO: fazer a leitura de um arquivo .txt, e chamar o lexer yyFlexLexer para identificar os tokens (OLHAR PÁGINA 35)*/
+/*  TODO: fazer a leitura de um arquivo .txt, e chamar o lexer yyFlexLexer para identificar os tokens (OLHAR PÁGINA 35 do SLIDE cc.03.lexica.pdf)*/
